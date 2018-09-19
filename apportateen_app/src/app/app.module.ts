de8f047dd4ca2f5742dfd20594479612d6ae1554
Carlos Serret <carloserret@gmail.com>
2018-09-18 21:56:15 -0500

    added enrollment module to appmodule and router

--- apportateen_app/src/app/app.module.ts
+++ apportateen_app/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './components/app/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UiModule } from './components/ui/ui.module';
+import { EnrollmentModule } from './components/enrollment/enrollment.module';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { UiModule } from './components/ui/ui.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    UiModule
+    UiModule,
+    EnrollmentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- apportateen_app/src/app/components/app/app-routing.module.ts
+++ apportateen_app/src/app/components/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
+import { EnrollmentComponent } from '../enrollment/enrollment-selector/enrollment-selector.component'
+
 const routes: Routes = [
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-    { path: 'inicio', component: HomeComponent }
+    { path: 'inicio', component: HomeComponent },
+    { path: 'ingresar', component: EnrollmentComponent }
 ]
 
 @NgModule({
