a9a30c18f69816917a23d21a8b8e9e426dcf98a0
Carlos Serret <carloserret@gmail.com>
2018-09-22 19:49:34 -0500

    added login and signup func

--- src/app/components/enrollment/signup/signup.component.ts
+++ src/app/components/enrollment/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ApiService } from '../../../services/api/api.service';
+import { User } from '../../../models/user.model';
 
 @Component({
     selector: 'signup',
@@ -6,4 +9,24 @@ import { Component } from '@angular/core';
     styleUrls: ['./signup.component.scss']
 })
 
-export class SignupComponent { }
+export class SignupComponent {
+    nombre: string;
+    correo: string;
+    password: string;
+    passwordConf: string;
+    fecha_nacimiento: string;
+    usuario: User = new User();
+    constructor(private firebaseAuth: AngularFireAuth, private api: ApiService) { }
+
+    signup() {
+        //missing field validation
+        this.usuario.nombre = this.nombre;
+        this.usuario.correo = this.correo;
+        this.usuario.fecha_nacimiento = '01-05-1996';
+        this.firebaseAuth.auth.createUserWithEmailAndPassword(this.correo, this.password).
+            then(user => {
+                this.usuario.user_id = user.user.uid;
+                this.api.createUser(this.usuario).subscribe(result => console.log(result));
+            })
+    }
+}
