a6d97c8a2ddbe4803e1b10ccd96d5fe03ad18fdd
Carlos Serret <carloserret@gmail.com>
2018-09-22 19:42:36 -0500

    added firebase and apiurl to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBns31xrzExqUbCC6nHOOjVswr6dq9vdY4',
+    authDomain: 'apportateen.firebaseapp.com',
+    databaseURL: 'https://apportateen.firebaseio.com',
+    projectId: 'apportateen',
+    storageBucket: 'apportateen.appspot.com',
+    messagingSenderId: '558841100052'
+  },
+  apiUrl: 'https://apportateen.herokuapp.com'
 };
