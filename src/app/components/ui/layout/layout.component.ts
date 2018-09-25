8662af9348a7631df67b8033a40632f9e2472a2d
Carlos Serret <carloserret@gmail.com>
2018-09-25 18:52:53 -0500

    hiding original navbar on admin module

--- src/app/components/ui/layout/layout.component.ts
+++ src/app/components/ui/layout/layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-layout',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-
-    constructor() { }
+    route: string;
+    constructor(loc: Location, private _router: Router) {
+        this._router.events.subscribe((val) => {
+            if (loc.path() != '') {
+                this.route = loc.path();
+            }
+        });
+    }
 
     ngOnInit() {
     }
-
 }
