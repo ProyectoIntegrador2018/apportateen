6ec4c01ac42802d8b60dd08c46c4c9a441ca82e6
Carlos Serret <carloserret@gmail.com>
2018-09-25 18:51:06 -0500

    created sedes an convocatoria component, empty

---
+++ src/app/components/admin/convocatorias/convocatorias.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'convocatorias',
+    templateUrl: './convocatorias.component.html',
+    styleUrls: ['./convocatorias.component.scss']
+})
+
+export class ConvocatoriasComponent { }
---
+++ src/app/components/admin/sedes/sedes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sedes',
+    templateUrl: './sedes.component.html',
+    styleUrls: ['./sedes.component.scss']
+})
+
+export class SedesComponent { }
