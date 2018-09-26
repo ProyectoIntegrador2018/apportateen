110db05d1a3cb428898ad6860069c3d4a1ccbd2a
Luis Felipe Salazar <luisfelipesv@users.noreply.github.com>
2018-09-26 01:30:50 -0500

    Created Sede dummy model and added some functionality to Sedes comp

--- src/app/components/admin/sedes/sedes.component.ts
+++ src/app/components/admin/sedes/sedes.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Sede } from '../../../models/sede.model';
 
 @Component({
     selector: 'sedes',
@@ -6,4 +7,40 @@ import { Component } from '@angular/core';
     styleUrls: ['./sedes.component.scss']
 })
 
-export class SedesComponent { }
+export class SedesComponent implements OnInit {
+
+  newSede: Sede;
+  selectedSede: Sede;
+
+  constructor() { }
+
+  ngOnInit() {
+      this.selectedSede = new Sede();
+      this.selectedSede.nombre = "Sede 1";
+      this.selectedSede.descripcion = "descripcion sede 1"
+  }
+
+  add() {
+      this.newSede = new Sede();
+      this.selectedSede = this.newSede;
+  }
+
+  delete(){
+
+  }
+
+  save(){
+
+  }
+
+  cancel() {
+    this.newSede = null;
+  }
+
+  create(){
+
+  }
+
+
+
+}
