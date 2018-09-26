fb9c781a718501dff10077850fc6b69e8d2b15ce
Luis Felipe Salazar <luisfelipesv@users.noreply.github.com>
2018-09-26 02:23:46 -0500

    List is dynamic now on Sedes

--- src/app/components/admin/sedes/sedes.component.ts
+++ src/app/components/admin/sedes/sedes.component.ts
@@ -11,13 +11,28 @@ export class SedesComponent implements OnInit {
 
   newSede: Sede;
   selectedSede: Sede;
+  sedes: Array<Sede> = [];
 
   constructor() { }
 
   ngOnInit() {
-      this.selectedSede = new Sede();
-      this.selectedSede.nombre = "Sede 1";
-      this.selectedSede.descripcion = "descripcion sede 1"
+      let sede1 = new Sede();
+      sede1.sede_id = "1";
+      sede1.nombre = "Sede 1";
+      sede1.descripcion = "Descripcion sede 1";
+      let sede2 = new Sede();
+      sede2.sede_id = "2";
+      sede2.nombre = "Sede 2";
+      sede2.descripcion = "Descripcion sede 2";
+      let sede3 = new Sede();
+      sede3.sede_id = "3";
+      sede3.nombre = "Sede 3";
+      sede3.descripcion = "Descripcion sede 3";
+
+      this.sedes.push(sede1);
+      this.sedes.push(sede2);
+      this.sedes.push(sede3);
+      this.selectedSede = sede1;
   }
 
   add() {
@@ -41,6 +56,11 @@ export class SedesComponent implements OnInit {
 
   }
 
+  select(sede: Sede){
+    this.selectedSede = sede;
+  }
+
+  
 
 
 }
