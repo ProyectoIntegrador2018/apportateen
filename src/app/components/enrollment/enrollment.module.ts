fc3097b5fe7748e3e640152f560b94aaf85fa6f4
Carlos Serret <carloserret@gmail.com>
2018-09-25 23:19:26 -0500

    changed input to material inputs and added dependencies to module

--- src/app/components/enrollment/enrollment.module.ts
+++ src/app/components/enrollment/enrollment.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EnrollmentComponent } from './enrollment-selector/enrollment-selector.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { ApiService } from '../../services/api/api.service';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MatIconModule } from '@angular/material';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         HttpModule,
-        HttpClientModule
+        HttpClientModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        ReactiveFormsModule,
+        MatIconModule
     ],
     providers: [ApiService],
     declarations: [EnrollmentComponent, LoginComponent, SignupComponent],
