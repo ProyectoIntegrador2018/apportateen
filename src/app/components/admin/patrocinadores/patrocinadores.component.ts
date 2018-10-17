2473d42143eb23c912bfb421bc133ef71856ca2e
Carlos Serret <carloserret@gmail.com>
2018-10-17 15:39:25 -0500

    empty categorias, sponsors and talleres components

---
+++ src/app/components/admin/patrocinadores/patrocinadores.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { Sede } from '../../../models/sede.model';
+import { ApiService } from '../../../services/api/api.service';
+
+@Component({
+  selector: 'patrocinadores',
+  templateUrl: './patrocinadores.component.html',
+  styleUrls: ['./patrocinadores.component.scss']
+})
+
+export class PatrocinadoresComponent implements OnInit {
+
+  newSede: Sede;
+  selectedSede;
+  sedes;
+
+  constructor(private api: ApiService) {
+    this.sedes = [];
+    this.selectedSede = {};
+  }
+
+  ngOnInit() {
+    this.api.getAllSedes().subscribe(result => {
+      this.sedes = result;
+      console.log(this.sedes);
+      this.autoSelect();
+    });
+  }
+
+  add() {
+    this.newSede = new Sede();
+    this.selectedSede = this.newSede;
+  }
+
+  delete() {
+    let index = this.sedes.indexOf(this.selectedSede);
+    this.sedes.splice(index, 1);
+    this.api.removeSede(this.selectedSede.id).subscribe(result => console.log(result));
+    this.autoSelect();
+  }
+
+  save() {
+    this.api.updateSede(this.selectedSede).subscribe(result => console.log(result));
+  }
+
+  cancel() {
+    this.newSede = null;
+    this.autoSelect();
+  }
+
+  create() {
+    this.sedes.push(this.selectedSede);
+    this.newSede = null;
+    this.api.createSede(this.selectedSede).subscribe(result => console.log(result));
+  }
+
+  select(sede: Sede) {
+    this.selectedSede = sede;
+  }
+
+  autoSelect() {
+    if (this.sedes.length != 0) {
+      this.selectedSede = this.sedes[0];
+    }
+  }
+
+
+
+}
