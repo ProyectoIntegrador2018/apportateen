0ac13e429b2a4c3d5aa0a6f36ff5075d83900c43
Carlos Serret <carloserret@gmail.com>
2018-10-24 18:45:17 -0500

    loading user permissions on page refresh

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { LOCAL_STORAGE, WebStorageService } from 'angular-webstorage-service';
+import { NgxPermissionsService } from 'ngx-permissions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ApportaTeen';
 
-  constructor(afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, public router: Router,
+    @Inject(LOCAL_STORAGE) private storage: WebStorageService,
+    private permissionsService: NgxPermissionsService) {
     afAuth.authState.subscribe(auth => {
       if (auth) {
         console.log('logged in');
@@ -17,5 +22,19 @@ export class AppComponent {
         console.log('not logged in');
       }
     });
+  }
+
+  ngOnInit() {
+    let perm = [];
+    this.afAuth.authState.subscribe(auth => {
+      if (auth) {
+        let user = this.storage.get('@user:data');
+        if (user) {
+          perm = user.isAdmin ? ["ADMIN"] : ["USER"];
+          console.log('loading permissions', perm);
+          this.permissionsService.loadPermissions(perm);
+        }
+      }
+    })
   }
 }
