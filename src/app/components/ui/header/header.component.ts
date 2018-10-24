85c53f92bf24c0d278f4568b0d8100df20cf775d
Carlos Serret <carloserret@gmail.com>
2018-10-24 18:48:02 -0500

    new header to main page

--- src/app/components/ui/header/header.component.ts
+++ src/app/components/ui/header/header.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { LOCAL_STORAGE, WebStorageService } from 'angular-webstorage-service';
+import { NgxPermissionsService } from 'ngx-permissions';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
     isCollapsed: boolean = false;
-    constructor() { }
+    user: boolean;
+    constructor(private afAuth: AngularFireAuth,
+        @Inject(LOCAL_STORAGE) private storage: WebStorageService,
+        private permissionsService: NgxPermissionsService,
+        public router: Router) {
+        this.user = false;
+    }
 
     ngOnInit() {
+        this.afAuth.authState.subscribe(auth => {
+            this.user = auth ? true : false;
+        })
+    }
+
+    logout() {
+        this.afAuth.auth.signOut().then(() => {
+            this.storage.remove('@user:data');
+            this.permissionsService.flushPermissions();
+        })
+    }
+
+    userRedirect() {
+        let stUser = this.storage.get('@user:data');
+        if (stUser.isAdmin) {
+            this.router.navigate(['admin']);
+        } else {
+            this.router.navigate(['usuario']);
+        }
     }
 
 }
