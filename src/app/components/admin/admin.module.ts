dbf2af463f0357ba49a52129eeeab8537919df34
Carlos Serret <carloserret@gmail.com>
2018-10-24 18:43:51 -0500

    added libraries to modules and added new dependencies

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -4,13 +4,14 @@ import { RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { FormsModule } from '@angular/forms';
 
-import { MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule, MatSlideToggleModule, MatButtonModule, MatInputModule, MatExpansionModule, MatSelectModule, MatDialogModule, MatSnackBarModule, } from '@angular/material'
+import { MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule, MatSlideToggleModule, MatButtonModule, MatInputModule, MatExpansionModule, MatSelectModule, MatDialogModule, MatSnackBarModule, MatMenuModule, } from '@angular/material'
 import { ConvocatoriasComponent } from './convocatorias/convocatorias.component';
 import { SedesComponent } from './sedes/sedes.component';
 import { TalleresComponent } from './talleres/talleres.component';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { PatrocinadoresComponent } from './patrocinadores/patrocinadores.component';
 import { AvisosComponent, ConfirmationDialog } from './avisos/avisos.component';
+import { StorageServiceModule } from 'angular-webstorage-service';
 
 @NgModule({
     imports: [
@@ -27,7 +28,9 @@ import { AvisosComponent, ConfirmationDialog } from './avisos/avisos.component';
         MatExpansionModule,
         MatSelectModule,
         MatDialogModule,
-        MatSnackBarModule
+        MatSnackBarModule,
+        StorageServiceModule,
+        MatMenuModule
     ],
     declarations: [MainComponent,
         ConvocatoriasComponent,
