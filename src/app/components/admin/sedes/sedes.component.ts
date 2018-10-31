b7face80cd690e047207d2c90ce23361ff59ce25
Carlos Serret <carloserret@gmail.com>
2018-10-31 12:26:15 -0600

    added sedes functionality

--- src/app/components/admin/sedes/sedes.component.ts
+++ src/app/components/admin/sedes/sedes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Sede } from '../../../models/sede.model';
 import { ApiService } from '../../../services/api/api.service';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { ConfirmationDialog } from 'app/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'sedes',
@@ -12,17 +14,20 @@ export class SedesComponent implements OnInit {
 
   newSede: Sede;
   selectedSede;
-  sedes;
+  sedes = [];
 
-  constructor(private api: ApiService) {
+  constructor(private api: ApiService, public dialog: MatDialog, public snackBar: MatSnackBar) {
     this.sedes = [];
     this.selectedSede = {};
   }
 
   ngOnInit() {
+    this.obtenersedes();
+  }
+
+  obtenersedes() {
     this.api.getAllSedes().subscribe(result => {
       this.sedes = result;
-      console.log(this.sedes);
       this.autoSelect();
     });
   }
@@ -33,14 +38,51 @@ export class SedesComponent implements OnInit {
   }
 
   delete() {
-    let index = this.sedes.indexOf(this.selectedSede);
-    this.sedes.splice(index, 1);
-    this.api.removeSede(this.selectedSede.id).subscribe(result => console.log(result));
-    this.autoSelect();
+    const dialogRef = this.dialog.open(ConfirmationDialog, {
+      disableClose: true
+    });
+    dialogRef.componentInstance.mensajeConfirmacion = `Se eliminará la sede ${this.selectedSede.nombre}. ¿Desea continuar?`;
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.api.removeSede(this.selectedSede.id).subscribe(res => {
+          if (res.status == 'success') {
+            this.obtenersedes();
+          }
+          this.snackBar.open(res.message, '', {
+            duration: 1000,
+          });
+        }, error => {
+          var errMessage = 'Ha sucedido un error eliminando la sede.'
+          if (error.error.indexOf('update or delete on table') > 0) {
+            errMessage = 'Error. Esta sede esta asignada a algún taller.'
+          }
+          this.snackBar.open(errMessage, '', {
+            duration: 1500,
+          });
+        });
+      }
+    });
   }
 
   save() {
-    this.api.updateSede(this.selectedSede).subscribe(result => console.log(result));
+    const dialogRef = this.dialog.open(ConfirmationDialog, {
+      disableClose: true
+    });
+    dialogRef.componentInstance.mensajeConfirmacion = `Se modificará la sede ${this.selectedSede.nombre}. ¿Desea continuar?`;
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.api.updateSede(this.selectedSede).subscribe(res => {
+          this.snackBar.open(res.message, '', {
+            duration: 1000
+          });
+          this.obtenersedes();
+        }, error => {
+          this.snackBar.open(error.error, '', {
+            duration: 1000
+          });
+        });
+      }
+    });
   }
 
   cancel() {
@@ -49,21 +91,29 @@ export class SedesComponent implements OnInit {
   }
 
   create() {
-    this.sedes.push(this.selectedSede);
     this.newSede = null;
-    this.api.createSede(this.selectedSede).subscribe(result => console.log(result));
+    this.api.createSede(this.selectedSede).subscribe(result => {
+      if (result.status == 'success') {
+        this.snackBar.open(result.message, '', {
+          duration: 1500,
+        });
+        this.obtenersedes();
+      }
+    }, error => {
+      this.snackBar.open(error.error, '', {
+        duration: 1500,
+      });
+    })
   }
 
   select(sede: Sede) {
-    this.selectedSede = sede;
+    this.selectedSede = Object.assign({}, sede);
   }
 
   autoSelect() {
     if (this.sedes.length != 0) {
-      this.selectedSede = this.sedes[0];
+      this.selectedSede = Object.assign({}, this.sedes[0]);
     }
   }
-
-
 
 }
