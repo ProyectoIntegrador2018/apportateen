0a8582ba4034a61aac473ce529a836709d021ce3
Vale Rocha <valerocha_96@hotmail.com>
2020-03-29 22:10:20 -0600

    routing para detalle taller

---
+++ src/app/components/user/detalle-taller/detalle-taller.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Inject} from '@angular/core';
+import { ApiService } from 'app/services/api/api.service';
+import { Taller } from 'app/models/taller.model';
+import { LOCAL_STORAGE, WebStorageService } from 'angular-webstorage-service';
+
+@Component({
+  selector: 'app-detalle-taller',
+  templateUrl: './detalle-taller.component.html',
+  styleUrls: ['./detalle-taller.component.scss']
+})
+export class DetalleTallerComponent implements OnInit {
+
+  talleres;
+  estatus;
+
+  constructor(private api: ApiService,
+    @Inject(LOCAL_STORAGE) private storage: WebStorageService,) {
+    this.talleres = [];
+    this.estatus = null;
+  }
+
+  ngOnInit() {
+    this.cargarTalleres();
+    this.cargarAvisos();
+  }
+
+  cargarAvisos(){
+    let userStorage = this.storage.get('@user:data');
+    console.log("user storage");
+    console.log(userStorage);
+
+  }
+
+  cargarTalleres() {
+    this.api.getAllTalleres().subscribe(result => {
+      this.talleres = result[0];
+      console.log("TALLERES");
+      console.log(this.talleres);
+    })
+  }
+
+}
