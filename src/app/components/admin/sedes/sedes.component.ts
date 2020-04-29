f74d2c39e2aaf9dc71617c0bbbed9480ab053e7e
Jonathan Cárdenas <JonathanCrd@users.noreply.github.com>
2020-04-29 01:49:06 -0500

    Modificar sede con responsable

--- src/app/components/admin/sedes/sedes.component.ts
+++ src/app/components/admin/sedes/sedes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Sede } from '../../../models/sede.model';
+import { Responsable } from '../../../models/responsable.model';
 import { ApiService } from '../../../services/api/api.service';
 import { MatDialog, MatSnackBar } from '@angular/material';
 import { ConfirmationDialog } from 'app/components/confirmation-dialog/confirmation-dialog.component';
@@ -14,6 +15,7 @@ export class SedesComponent implements OnInit {
 
   newSede: Sede;
   selectedSede;
+  originalInfoSede: Sede;
   sedes = [];
 
   constructor(private api: ApiService, public dialog: MatDialog, public snackBar: MatSnackBar) {
@@ -72,24 +74,66 @@ export class SedesComponent implements OnInit {
     dialogRef.componentInstance.mensajeConfirmacion = `Se modificará la sede seleccionada. ¿Desea continuar?`;
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        if (this.selectedSede == undefined) {
-          console.log("LA SEDE NO TIENE RESPONSABLE")
-        }
-        this.api.createResponsable(this.selectedSede).subscribe(res => {
-          this.api.getReponsable(this.selectedSede).subscribe(res_id => {
-            this.selectedSede.responsable = res_id.id_responsable;
-            this.api.updateSede(this.selectedSede).subscribe(res => {
-              this.snackBar.open(res.message, '', {
-                duration: 1000
-              });
-              this.obtenersedes();
+        if (this.originalInfoSede.correo_responsable!= null) {
+          // La sede ya tenía responsable
+          if(this.selectedSede.correo_responsable == ""){
+            // Se elimino el responsable
+
+          } 
+          else if (this.originalInfoSede.correo_responsable != this.selectedSede.correo_responsable || this.originalInfoSede.correo_responsable != this.selectedSede.correo_responsable){
+            // Se tiene que hacer update al responsable
+            var data = new Responsable
+            data.id_responsable = this.selectedSede.id_responsable
+            data.nombre_responsable = this.selectedSede.nombre_responsable
+            data.correo_responsable = this.selectedSede.correo_responsable
+
+            this.api.updateResponsable(data).subscribe(res => {
+
             }, error => {
-              this.snackBar.open(error.error, '', {
+              this.snackBar.open('Problema al actualizar el responsable', '', {
                 duration: 1000
               });
+            })
+          }
+        }
+        else if(this.selectedSede.correo_responsable != "") {
+          // No tiene responsable y se agregó uno
+          var data = new Responsable
+          data.id_responsable = null
+          data.nombre_responsable = this.selectedSede.nombre_responsable
+          data.correo_responsable = this.selectedSede.correo_responsable
+
+          this.api.createResponsable(data).subscribe(res => {
+            this.api.getReponsable(this.selectedSede).subscribe(res => {
+              this.selectedSede.responsable = res.id_responsable;
+            }, error => {
+              console.log(error.error)
+              this.snackBar.open("Error al obtener el responsable", '', {
+                duration: 1000
+              });
+            })
+          }, error => {
+            console.log(error.error)
+            this.snackBar.open("Error al crear responsable", '', {
+              duration: 1000
             });
+          })
+          
+        }
+
+        console.log("UPADTING SEDE")
+        // Update de la sede
+        this.api.updateSede(this.selectedSede).subscribe(res => {
+          this.snackBar.open(res.message, '', {
+            duration: 1000
+          });
+          this.obtenersedes();
+        }, error => {
+          this.snackBar.open(error.error, '', {
+            duration: 1000
           });
         });
+
       }
     });
   }
@@ -104,7 +148,12 @@ export class SedesComponent implements OnInit {
 
     // Checar si la sede tiene responsable
     if (this.selectedSede.correo_responsable != "") {
-      this.api.createResponsable(this.selectedSede).subscribe(res => {
+      var responsable = new Responsable
+      responsable.id_responsable = this.selectedSede.id_responsable
+      responsable.nombre_responsable = this.selectedSede.nombre_responsable
+      responsable.correo_responsable = this.selectedSede.correo_responsable
+
+      this.api.createResponsable(responsable).subscribe(res => {
         this.api.getReponsable(this.selectedSede).subscribe(res_id => {
           this.selectedSede.responsable = res_id.id_responsable;
           this.api.createSede(this.selectedSede).subscribe(result => {
@@ -137,7 +186,6 @@ export class SedesComponent implements OnInit {
     }
     else {
       // La sede no tiene responsable
-      console.log("NO HAY RESPONSABLE WUUW")
       this.selectedSede.responsable = null
       this.api.createSede(this.selectedSede).subscribe(result => {
         if (result.status == 'success') {
@@ -154,29 +202,23 @@ export class SedesComponent implements OnInit {
       })
 
     }
-    // this.api.createSede(this.selectedSede).subscribe(result => {
-    //   if (result.status == 'success') {
-    //     this.snackBar.open(result.message, '', {
-    //       duration: 1500,
-    //     });
-    //     this.obtenersedes();
-    //   }
-    // }, error => {
-    //   this.snackBar.open(error.error, '', {
-    //     duration: 1500,
-    //   });
-    // })
   }
 
   select(sede: Sede) {
     this.selectedSede = Object.assign({}, sede);
     this.newSede = null;
+    this.originalInfoSede = Object.assign({},this.selectedSede)
   }
 
   autoSelect() {
     if (this.sedes.length != 0) {
       this.selectedSede = Object.assign({}, this.sedes[0]);
+      this.originalInfoSede = Object.assign({},this.selectedSede)
     }
+  }
+
+  show(){
+    console.log(this.selectedSede)
   }
 
 }
---
+++ src/app/models/responsable.model.ts
@@ -0,0 +1,5 @@
+export class Responsable {
+    id_responsable: number = null;
+    nombre_responsable: string = '';
+    correo_responsable: string = '';
+}
