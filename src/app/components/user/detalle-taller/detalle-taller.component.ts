950dea47d681d71980d1995ecfce49bf67a7a19f
Flor Barbosa <esthelabarbosa@gmail.com>
2020-05-01 13:07:49 -0500

    Termina detalles de front en detalle de un taller (#108)
    facil-nav

--- src/app/components/user/detalle-taller/detalle-taller.component.ts
+++ src/app/components/user/detalle-taller/detalle-taller.component.ts
@@ -28,6 +28,8 @@ export class DetalleTallerComponent implements OnInit {
   user: User = new User();
   talleres;
   checa_talleres: boolean;
+  fecha_inicio;
+  fecha_fin;
 
   constructor(private api: ApiService,
     @Inject(LOCAL_STORAGE) private storage: WebStorageService,
@@ -39,6 +41,8 @@ export class DetalleTallerComponent implements OnInit {
     this.estatus = null;
     this.checa_talleres = true;
     this.talleres = [];
+    this.fecha_inicio;
+    this.fecha_fin;
   }
 
   ngOnInit() {
@@ -55,6 +59,9 @@ export class DetalleTallerComponent implements OnInit {
     this.user = this.storage.get('@user:data');
 
     window.scrollTo(0, 0);
+
+    
+
   }
 
 
@@ -70,9 +77,35 @@ export class DetalleTallerComponent implements OnInit {
     this.api.getTaller(this.idTaller).subscribe(result => {
       this.taller = result[0][0];
       this.taller["inscritos"] = result[1][0]["inscritos"];
+
+      this.fecha_fin = this.taller.fecha_fin;
+      this.fecha_inicio = this.taller.fecha_inicio;
+
+      
+      this.fecha_inicio = this.formatDate(this.fecha_inicio.slice(0,10));
+      this.fecha_fin = this.formatDate(this.fecha_fin.slice(0,10));
+
     })
+
+
   }
 
+  formatDate(x:string) {
+
+    let dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    let date = new Date(x.replace(/-+/g, '/'));
+  
+    var fechaNum = date.getDate();
+    var mes_name = date.getMonth();
+  
+  
+    return(dias[date.getDay()-1] + " " + fechaNum + " de " + meses[mes_name] + " de " + date.getFullYear());
+  
+  }
+
+  
   obtenerCostos() {
     this.api.getCostos().subscribe(result => {
       this.costosPorEscuela = result;
@@ -98,8 +131,6 @@ export class DetalleTallerComponent implements OnInit {
   let t : any;
   for(t in this.user.talleres){
 
-    console.log(this.user.talleres);
-    console.log(this.talleres);
     let tall = this.talleres.find(x => x.id === this.user.talleres[t]);
 
 
