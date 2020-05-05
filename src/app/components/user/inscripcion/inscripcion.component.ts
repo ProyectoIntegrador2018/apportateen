2a43a18257f54d7625979a6bafe27bdb843cdfdb
Flor Barbosa <esthelabarbosa@gmail.com>
2020-05-04 20:26:10 -0500

    facil-nav en el sistema (#114)
    * First commit
    
    Branch de fácil navegación dentro del sistema para permitir que el usuario registre más de un solo taller
    
    * Elimina un taller y agrega varios en detalle taller
    
    * Cambios para mostrar todos los talleres
    
    * Agrega restricciones de fecha, hora y ciudad para inscribir otro taller
    
    * Funcionalidad en botón para ver talleres del usuario
    
    * Agrega detales en la interfaz de usuario para facilitar inscripción
    
    * Cambia portada de detalle taller y arregla carousel que se salía de la página
    
    * Update inscripcion.component.html
    
    * Termina detalles en detalle-taller

--- src/app/components/user/inscripcion/inscripcion.component.ts
+++ src/app/components/user/inscripcion/inscripcion.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, NgModule } from '@angular/core';
 import { ApiService } from 'app/services/api/api.service';
 import { Sede } from 'app/models/sede.model';
 import { LOCAL_STORAGE, WebStorageService } from 'angular-webstorage-service';
 import { User } from 'app/models/user.model';
 import { Taller } from 'app/models/taller.model';
-import { MatDialog, MatSnackBar,MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { MatDialog, MatSnackBar,MAT_DIALOG_DATA, MatDialogRef, MatSelectModule } from '@angular/material';
 import { ConfirmationDialog } from 'app/components/confirmation-dialog/confirmation-dialog.component';
+import { WarningDialogComponent } from 'app/components/warning-dialog/warning-dialog.component';
 import { TalleresComponent } from 'app/components/admin/talleres/talleres.component';
 
 export interface DialogData{
 
 }
 
+@NgModule({
+  imports:[MatSelectModule]
+})
 @Component({
   selector: 'app-inscripcion',
   templateUrl: './inscripcion.component.html',
@@ -20,10 +24,16 @@ export interface DialogData{
 export class InscripcionComponent implements OnInit {
   estatus: boolean;
   user: User = new User();
-  tallerActual;
+  // tallerActual;
   talleres;
   sedes;
   selectedSede: Sede = new Sede();
+  sede_seleccionada : boolean;
+  muestra_todos : boolean;
+  checa_talleres: boolean;
+  talleres_usuario : Taller[];
+  muestra_tusuario: boolean;
+  sedeselect;
 
   constructor(private api: ApiService,
     @Inject(LOCAL_STORAGE) private storage: WebStorageService,
@@ -31,13 +41,22 @@ export class InscripcionComponent implements OnInit {
     public snackBar: MatSnackBar) {
     this.talleres = [];
     this.sedes = [];
-    this.tallerActual = '';
+    // this.tallerActual = '';
     this.estatus = null;
+    this.sede_seleccionada = null;
+    this.muestra_todos = true;
+    
+    this.talleres_usuario = [];
+    this.checa_talleres = true;
+    this.muestra_tusuario;
   }
 
   ngOnInit() {
     this.user = this.storage.get('@user:data');
     this.cargarSedes();
+    this.cargarTalleres();
+    
+
   }
 
   cargarSedes() {
@@ -46,21 +65,78 @@ export class InscripcionComponent implements OnInit {
       if (this.estatus) {
         this.api.getAllSedes().subscribe(result => {
           this.sedes = result;
-          if (this.user.idtaller != 0) {
-            this.obtenerTallerActual();
-          }
+          // if (this.user.idtaller != 0) {
+          //   this.obtenerTallerActual();
+          // }
         })
       }
     })
 
   }
 
+  cargarTalleres(){
+    this.api.getAllTalleres().subscribe(result => {
+      this.talleres = result[0];
+      this.muestra_todos = true;
+      this.muestra_tusuario = false;
+      
+      // this.cargarTalleresUsuario();
+      this.sedeselect = undefined;
+      this.cargaTu();
+    });
+
+    
+    
+  }
+
+  cargaTu(){
+
+    let t:any;
+    this.talleres_usuario = [];
+
+    console.log(this.user.talleres);
+    console.log(this.talleres);
+    for(t in this.user.talleres){
+      
+      let temp = this.talleres.find(x => x.id === this.user.talleres[t]);
+      
+      this.talleres_usuario.push(temp);
+      
+      
+    }
+    
+  }
+
+  cargarTalleresUsuario(){
+    let t:any;
+    
+    console.log(this.talleres_usuario.length + " " +this.user.talleres.length);
+    
+    // if(!(this.talleres_usuario.length === this.user.talleres.length)){
+    //   this.talleres_usuario = [];
+    //   for(t in this.user.talleres){
+        
+    //     this.talleres_usuario.push(this.talleres.find(x => x.id === this.user.talleres[t]));
+    //     // console.log("hola "+ this.talleres_usuario);
+    //   }
+    // }
+
+    this.cargaTu();
+    
+    this.muestra_todos = false;
+    this.muestra_tusuario = true;
+    this.sedeselect = undefined;
+  }
+
   seleccionarSede(event: any) {
     this.selectedSede = this.sedes.find(x => x.id === event.value);
     this.selectedSede.talleres = this.selectedSede.talleres.filter(x => x.categoria === this.user.idcategoria);
+    this.sede_seleccionada = true;
+    this.muestra_todos = false;
+    this.muestra_tusuario = false;
   }
 
-  quitarInscripcion() {
+  quitarInscripcion(taller: Taller) {
     const dialogRef = this.dialog.open(ConfirmationDialog, {
       disableClose: true
     });
@@ -74,11 +150,22 @@ export class InscripcionComponent implements OnInit {
             duration: 900,
           });
         });
-        this.user.idtaller = 0;
-        this.user.id_axtuser = "";
+        
+        this.user.id_axtuser = ""; // para qué es este id ??
+        
+        const index_taller = this.user.talleres.indexOf(taller.id);
+        
+        if(index_taller > -1){
+          this.user.talleres.splice(index_taller,1);
+        }
+
+        this.cargaTu();
+
+
+          
         this.api.updateUser(this.user).subscribe(res => {
           this.storage.set('@user:data', this.user);
-          this.tallerActual = '';
+          // this.tallerActual = '';
           this.snackBar.open(res.message, '', {
             duration: 1500,
           });
@@ -92,19 +179,68 @@ export class InscripcionComponent implements OnInit {
   }
 
   inscripcion(taller: Taller) {
-    const dialogRef = this.dialog.open(ConfirmationDialog, {
-      disableClose: true
-    });
-    let message = `Está por inscribirse al taller ${taller.nombre}. ¿Desea continuar?`;
-    if (this.user.idtaller != 0) {
-      message = `Se identificó que ya estas registrado en otro taller. Si desea estar inscrito simultáneamente en dos o más talleres, deberas crear un nuevo usuario por cada nuevo registro que deseaa realizar. En caso de querer reemplazar el taller inscrito actual, se reemplazará la inscripción actual por el taller ${taller.nombre}. ¿Desea continuar?`;
+
+    // sección para checar que el usuario puede inscribir otro taller dependiendo del horario y fecha de los talleres que ya tiene inscritos
+    let t : any;
+    for(t in this.talleres_usuario){
+      
+      // let tall = this.talleres.find(x => x.id === this.user.talleres[t]);
+    
+      var fi= this.talleres_usuario[t].fecha_inicio.slice(0,10);
+      var ff = this.talleres_usuario[t].fecha_fin.slice(0,10);
+      var fi_n = taller.fecha_inicio.slice(0,10);
+      var ff_n = taller.fecha_fin.slice(0,10);
+      
+      fi = fi.split("-").join("");
+      ff = ff.split("-").join("");
+      fi_n = fi_n.split("-").join("");
+      ff_n = ff_n.split("-").join("");
+      
+      var hi = this.talleres_usuario[t].hora_inicio.replace(":","");
+      var hf = this.talleres_usuario[t].hora_fin.replace(":","");
+      var hi_n = taller.hora_inicio.replace(":","");
+      var hf_n = taller.hora_fin.replace(":","");
+
+      // checar si los rangos de fechas del nuevo taller a inscribir estan dentro de los rangos de fechas de los talleres ya inscritos
+      // TODO: más pruebas de esto
+
+      if((fi_n >= fi && ff >= fi_n) || (ff_n >= fi && ff >= ff_n)){
+        // checa si las horas del nuevo taller coinciden dentro de las horas de los talleres que ya tiene inscritos
+
+        if((hi_n >= hi && hf >= hi_n) || (hf_n >= hi && hf >= hf_n) || taller.estado != this.talleres_usuario[t].estado){
+          this.checa_talleres = false;
+        }
+      }
+
     }
 
-    dialogRef.componentInstance.mensajeConfirmacion = message;
+
+    let dialogRef, message;
+    
+    if(!this.checa_talleres){
+      dialogRef = this.dialog.open(WarningDialogComponent);
+      
+    } else {
+      
+      dialogRef = this.dialog.open(ConfirmationDialog, {
+        disableClose: true
+      });
+  
+      message = `Está por inscribirse al taller ${taller.nombre} en el estado de ${taller.estado}. ¿Desea continuar?`;
+      dialogRef.componentInstance.mensajeConfirmacion = message;
+    }
+   
     dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.user.idtaller = taller.id;
+      if (result && this.checa_talleres) {
+        // this.user.idtaller = taller.id;
+        this.user.talleres.push(taller.id);
+        
+        this.cargaTu();
+        
+
+
         this.user.id_axtuser = (this.selectedSede.nombre).toUpperCase() + "-" + (taller.nombre) + taller.inscritos;
+        
         if (this.selectedSede.nombre === "SOFTTEK" || this.selectedSede.nombre === "UDEM") {
           this.user.num_conf_pago = "BECA";
           this.api.updateUsuarioNumConfPago(this.user).subscribe(res => {
@@ -114,9 +250,10 @@ export class InscripcionComponent implements OnInit {
             });
           });
         }
+
         this.api.updateUser(this.user).subscribe(res => {
           this.storage.set('@user:data', this.user);
-          this.obtenerTallerActual()
+          // this.obtenerTallerActual()
           this.cargarSedes();
           this.snackBar.open(res.message, '', {
             duration: 1500,
@@ -126,13 +263,9 @@ export class InscripcionComponent implements OnInit {
             duration: 900,
           });
         })
+      } else{
+        this.checa_talleres = true; //resetea valor
       }
     })
   }
-
-  obtenerTallerActual() {
-    let sede = this.sedes.find(sede => sede.talleres.some(item => item.id === this.user.idtaller));
-    this.tallerActual = `${sede.talleres.find(x => x.id === this.user.idtaller).nombre} - ${sede.nombre}`;
-  }
-
 }
