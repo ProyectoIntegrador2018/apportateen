b2657c2f0f2f62b3e057422a8794b54f7c46cc63
Vale Rocha <valerocha_96@hotmail.com>
2020-05-17 13:00:48 -0500

    arreglar desplegar monto ficha de pago

--- src/app/components/user/talleres-inscritos/talleres-inscritos.component.ts
+++ src/app/components/user/talleres-inscritos/talleres-inscritos.component.ts
@@ -16,6 +16,8 @@ export class TalleresInscritosComponent implements OnInit {
   talleres;
   isTalleresInscritos;
   user: User = new User();
+  currentCost;
+  print;
 
   constructor(private api: ApiService,
     @Inject(LOCAL_STORAGE) private storage: WebStorageService,
@@ -23,11 +25,14 @@ export class TalleresInscritosComponent implements OnInit {
     public snackBar: MatSnackBar) {
     this.talleres = [];
     this.isTalleresInscritos = false;
+    this.print = true;
   }
 
+  //para un usuario, todos los talleres tienen el mismo costo porque depende de su tipo de escuela, y los que sono gratis ya estaran en la seccion de talleres inscritos
   ngOnInit() {
     this.user = this.storage.get('@user:data');
     this.cargarTalleresInscritos();
+    this.obtenerCostos();
   }
 
   cargarTalleresInscritos() {
@@ -38,16 +43,26 @@ export class TalleresInscritosComponent implements OnInit {
     });
   }
 
-  imprimirFichaPago() {
-    //opcion 1 con css,  pero tendria que poner la clase no print me en el main user component
-    window.print();
 
-    //opcion 2
-    // var printContents = document.getElementById('fichaPago').innerHTML;
-    // var originalContents = document.body.innerHTML;
-    // document.body.innerHTML = printContents;
-    // window.print();
-    // document.body.innerHTML = originalContents;
+  obtenerCostos() {
+    this.api.getCostos().subscribe(result => {
+      let costosPorEscuela = result;
+      if (this.user.escuela_tipo == "Privada") {
+        this.currentCost = costosPorEscuela["escuela_privada"];
+      } else {
+        this.currentCost = costosPorEscuela["escuela_publica"];
+      }
+    });
+  }
+
+
+  imprimirFichaPago(taller: Taller) {
+    //opcion 1 con css
+    // this.print = false;
+    // // this.currentCost = this.costoTaller(taller);
+    // this.currentCost = 500;
+    // this.print = true;
+    window.print();
   }
 
   toggleButton(direction: String) {
