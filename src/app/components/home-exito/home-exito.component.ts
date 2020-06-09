acea56b5c4a9e8e76350619940b912b0fb91de9b
Flor Barbosa <esthelabarbosa@gmail.com>
2020-06-09 11:55:39 -0500

    Agrega historias de éxito y patrocinadores

--- src/app/components/home-exito/home-exito.component.ts
+++ src/app/components/home-exito/home-exito.component.ts
@@ -15,17 +15,23 @@ export class HomeExitoComponent {
 
     constructor(public snackBar: MatSnackBar, private api: ApiService) {
         this.testimonios = [{
-          nombre:'Lizzie Hernández',
-          subtitulo:'Generación 2015',
-          testimonio:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis velit erat. Donec aliquam nibh quis rutrum auctor. Nullam ac facilisis lacus. Vivamus hendrerit, urna mollis faucibus maximus, lorem justo venenatis erat, ut efficitur sapien arcu vitae felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec eros consectetur, pharetra urna id, semper elit.',
-          urlImg: '../../../assets/img/historiaExito.PNG'
+          nombre:'Carlos Aquino Colín',
+          subtitulo:'Edad: 11 años, estudiante de primero de secundaria',
+          testimonio:'Inició desde los 9 años y ha aprendido diseño de apps básicas y avanzadas, curso de drones, impresión y diseño en 3D, programación competitiva y robótica. Piensa que todos ellos le servirán mucho en su formación profesional de universidad y en su carrera.',
+          // urlImg: '../../../assets/img/h-exito/historiaExito.PNG'
         },
         {
-          nombre:'Ana María López',
-          subtitulo:'Generación 2017',
-          testimonio:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis velit erat. Donec aliquam nibh quis rutrum auctor. Nullam ac facilisis lacus. Vivamus hendrerit, urna mollis faucibus maximus, lorem justo venenatis erat, ut efficitur sapien arcu vitae felis.',
-          urlImg: '../../../assets/img/historiaExito.PNG'
-        }
+          nombre:'César Ricardo Garza',
+          subtitulo:'Edad: 13 años',
+          testimonio:'¡¡Es increíble como así de fácil aprendimos a hacer apps en Android!!',
+          // urlImg: '../../../assets/img/h-exito/historiaExito.PNG'
+        },
+        {
+          nombre:'Eugenio Treviño, Jorge Salvans, Jesús Garza y Ángel Salvans',
+          subtitulo:'Integrantes del equipo ABC aprende',
+          testimonio:'Participantes del taller App Inventor de Axt@teen 2019 y creadores de una app educativa que participó en la Feria de Ciencia y Tecnología 2019.',
+          urlImg: '../../../assets/img/h-exito/he-3.jpg'
+        },
       ];
     }
 
